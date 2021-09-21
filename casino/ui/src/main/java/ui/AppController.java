f0bbafad2a35ff48d1a5c589a4c627784df96cc2
Nilashan Arul <nilashan.arul@gmail.com>
2021-09-21 10:33:15 +0200

    added some test functions to UI

--- casino/ui/src/main/java/ui/AppController.java
+++ casino/ui/src/main/java/ui/AppController.java
@@ -2,9 +2,50 @@ package ui;
 
 import core.App;
 
+import com.jfoenix.controls.JFXButton;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.*;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 
 public class AppController {
+    
+    //------------Elements-----------------//
+
+    @FXML
+    private TextField signInUser;
+
+    @FXML
+    private PasswordField signInPassword;
+
+    @FXML
+    private JFXButton signInBtn;
+
+    @FXML
+    private JFXButton newUserBtn;
+
+    
+    //------------Functions-----------------//
+    
+    @FXML
+    void onNewUserClick(ActionEvent event) {
+        Alert a = new Alert(AlertType.NONE);
+        a.setAlertType(AlertType.CONFIRMATION);
+        a.setContentText("Confirms that create new user button clicked");
+        a.show();
+    }
+
+    @FXML
+    void onSignInClick(ActionEvent event) {
+        Alert a = new Alert(AlertType.NONE);
+        a.setAlertType(AlertType.CONFIRMATION);
+        a.setContentText("Confirms that sign in button clicked");
+        a.show();
+        
+    }
+
     
 }
