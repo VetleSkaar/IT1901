2e8c42c1198f311b9418c0ed66f6a2807175bc9f
Petter Næss Evjen <petterev@stud.ntnu.no>
2021-09-23 11:55:22 +0200

    Fixed player logic

--- casino/core/src/main/java/core/Player.java
+++ casino/core/src/main/java/core/Player.java
@@ -1,35 +1,34 @@
+package core;
+
 public class Player {
     private String name;
     private int balance;
 
-
-    public Player(String name){
-        if(name.isBlank()){
+    public Player(String name) {
+        if (name.isBlank()) {
             throw new IllegalArgumentException("Player name cannot be blank.");
         }
         this.name = name;
     }
 
-    public Player(String name, int balance){
-        super(name);
-        if(balance<=0){
+    public Player(String name, int balance) {
+        this(name);
+        if (balance <= 0) {
             throw new IllegalArgumentException("Player balance must be positive.");
         }
-        this.balance=balance;
+        this.balance = balance;
     }
 
-
-    public string getName(){
+    public String getName() {
         return this.name;
     }
 
-    public int getBalance(){
+    public int getBalance() {
         return this.balance;
     }
 
-
-    public updateBalance() {
-        //todo
+    public void updateBalance() {
+        // todo
     }
 
 }
