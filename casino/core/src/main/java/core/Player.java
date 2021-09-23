85f0f28e274350371ddbbeee864fa4d61d7f9ab4
Petter Næss Evjen <petterev@stud.ntnu.no>
2021-09-23 10:59:56 +0200

    Simple player logic

---
+++ casino/core/src/main/java/core/Player.java
@@ -0,0 +1,32 @@
+public class Player {
+    private String name;
+    private int balance;
+
+
+    public Player(String name){
+        if(name.isBlank()){
+            throw new IllegalArgumentException("Player name cannot be blank.");
+        }
+        this.name = name;
+    }
+
+    public Player(String name, int balance){
+        super(name);
+        if(balance<=0){
+            throw new IllegalArgumentException("Player balance must be positive.");
+        }
+        this.balance=balance;
+    }
+
+
+    public string getName(){
+        return this.name;
+    }
+
+    public int getBalance(){
+        return this.balance;
+    }
+
+
+
+}
