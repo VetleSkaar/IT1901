daeb66a6f97774568a39b8205a5a7d51852ce116
Petter Næss Evjen <petterev@stud.ntnu.no>
2021-09-23 23:59:18 +0200

    Almost works

--- casino/core/src/test/java/core/testPlaceHolder.java
+++ casino/core/src/test/java/core/testPlaceHolder.java
@@ -1,7 +1,7 @@
 package core;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Assertions;
+// import org.junit.jupiter.api.Test;
 
 public class testPlaceHolder {
 
