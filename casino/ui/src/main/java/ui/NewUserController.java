434ce583e6143cfba83f99c4c682e0e40caf32af
Nilashan Arul <nilashan.arul@gmail.com>
2021-10-10 13:49:59 +0200

    added form validation to NewUser

--- casino/ui/src/main/java/ui/NewUserController.java
+++ casino/ui/src/main/java/ui/NewUserController.java
@@ -1,15 +1,23 @@
 package ui;
 
+import java.time.LocalDate;
+
 import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXCheckbox;
 import io.github.palexdev.materialfx.controls.MFXDatePicker;
 import io.github.palexdev.materialfx.controls.MFXLabel;
 import io.github.palexdev.materialfx.controls.MFXPasswordField;
 import io.github.palexdev.materialfx.controls.MFXTextField;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class NewUserController {
 
@@ -32,6 +40,46 @@ public class NewUserController {
     private MFXButton newUserCreateBtn;
 
     @FXML
+    void validation(){
+        LocalDate minAge = LocalDate.now().minusYears(18);
+        try{
+            if(dateOfBirth.getDate().isAfter(minAge)){
+                Alert dateFail = new Alert(AlertType.ERROR);
+                dateFail.setHeaderText("Failure");
+                dateFail.setContentText("You must be 18 or older");
+                dateFail.showAndWait();
+        }
+        } catch(NullPointerException e){
+            Alert dateFail = new Alert(AlertType.ERROR);
+                dateFail.setHeaderText("Failure");
+                dateFail.setContentText("Please enter your date of birth");
+                dateFail.showAndWait();
+        }
+        if(newUserName.getText().isEmpty()){
+            Alert nameFail = new Alert(AlertType.ERROR);
+            nameFail.setHeaderText("Failure");
+            nameFail.setContentText("You must type something as username");
+            nameFail.showAndWait();
+        }
+        if(newUserPassword.getText().isEmpty()){
+            Alert passFail = new Alert(AlertType.ERROR);
+            passFail.setHeaderText("Failure");
+            passFail.setContentText("You must type something as password");
+            passFail.showAndWait();
+        }
+    }
+
+    @FXML
+    void checkIfAgreed() {
+        if(termsCheckBox.isSelected()){
+            newUserCreateBtn.setDisable(false);
+        }
+        else {
+            newUserCreateBtn.setDisable(true);
+        }
+    }
+
+    @FXML
     void onTermsBtnClick(MouseEvent event) {
         Alert a = new Alert(AlertType.NONE);
         a.setAlertType(AlertType.INFORMATION);
@@ -40,4 +88,8 @@ public class NewUserController {
 
     }
 
+    @FXML
+    void onNewUserBtnClick(ActionEvent event) {
+        validation();
+    }
 }
