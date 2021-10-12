a59267bcbb115033a24e0394bab5c21cdc0eeaf8
Vetle Skaar <vetls@stud.ntnu.no>
2021-10-12 03:03:48 +0200

    Basic write data method

--- casino/core/src/main/java/core/FileHandling.java
+++ casino/core/src/main/java/core/FileHandling.java
@@ -1,83 +1,31 @@
 package core;
 
-// import java.io.*;
-// import java.util.ArrayList;
-// import java.util.List;
+import java.io.IOException;
+import java.util.List;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
 public class FileHandling {
-    // private final String FILE_PATH;
-    // private final User userId; // Temporary name for data type. Type to be used
-    // for users in the casino
 
-    // public FileHandling(Player userId) {
-    // this.userId = userId;
-    // this.FILE_PATH = userId.getName() + "_user_data.txt";
-    // }
+    public Player player;
 
-    // /**
-    // * Temporary code to be edited later.
-    // *
-    // * Assumes a user with a name or id, and a balance to be read/written to a
-    // .txt
-    // * file. May use a different system to store information in the future.
-    // *
-    // * Currently working with a single user.
-    // *
-    // * TODO: - Add support for several userID`s - Integrate with user handling
-    // * system
-    // *
-    // * @return
-    // * @throws IOException
-    // */
+    public void readData() {
+        
+    }
 
-    // @Override
-    // public Player readUserState() throws IOException {
-    // List<String> userData = new ArrayList<>();
+    public void writeData() {
+        ObjectMapper mapper = new ObjectMapper();
+        
+        try {
+            mapper.writeValue(new File(playerData.json), player);
 
-    // try (BufferedReader br = new BufferedReader(new FileReader(FILE_PATH))) {
-    // String line;
-    // while ((line = br.readLine()) != null) {
-    // userData.add(line);
-    // }
-    // } catch (IOException e) {
-    // writePlayerState();
-    // return new User();
-    // }
-
-    // String name = userData.get(0);
-    // if (name.isBlank()) {
-    // throw new IllegalArgumentException("Invalid file. No user ID found.");
-    // }
-
-    // if (!userData.get(1).matches("[0-9]+")) {
-    // throw new IllegalArgumentException("Invalid file. User balance is not an
-    // integer.");
-    // } else {
-    // int balance = Integer.parseInt(userData.get(1));
-
-    // if (userId.isNegative(balance)) {
-    // throw new IllegalArgumentException("Invalid file. User balance must be
-    // positive.");
-    // } else {
-    // return new User();
-    // }
-    // }
-    // }
-
-    // @Override
-    // public boolean writePlayerState() {
-    // try {
-    // FileWriter fw = new FileWriter(FILE_PATH);
-    // String playerData = userId.getName() + "\n" + userId.getBalance();
-
-    // fw.write(playerData);
-    // fw.close();
-
-    // return true;
-
-    // } catch (IOException e) {
-    // e.printStackTrace();
-    // }
-    // return false;
-    // }
+            String jsonString = mapper.writeValueAsString(player);
+            System.out.println(jsonString);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        om.writeValue(new File("playerData.json"), player);
+    }
 }
