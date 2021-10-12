9621de6737238148c97b4f553c79040469929850
Vetle Skaar <vetls@stud.ntnu.no>
2021-10-12 03:14:31 +0200

    Simple readData method.

--- casino/core/src/main/java/core/FileHandling.java
+++ casino/core/src/main/java/core/FileHandling.java
@@ -11,21 +11,25 @@ public class FileHandling {
     public Player player;
 
     public void readData() {
-        
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            player = mapper.readValue(new File("playerData.json"), Player.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void writeData() {
         ObjectMapper mapper = new ObjectMapper();
         
         try {
-            mapper.writeValue(new File(playerData.json), player);
+            mapper.writeValue(new File("playerData.json"), player);
 
             String jsonString = mapper.writeValueAsString(player);
             System.out.println(jsonString);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
-        om.writeValue(new File("playerData.json"), player);
     }
 }
