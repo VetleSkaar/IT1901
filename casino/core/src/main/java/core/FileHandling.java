3b1c2e3cb1acecb1a60c65fc2ca6c259f1a99397
Vetle Skaar <vetls@stud.ntnu.no>
2021-10-13 12:44:41 +0200

    Write to JSON method functional.

--- casino/core/src/main/java/core/FileHandling.java
+++ casino/core/src/main/java/core/FileHandling.java
@@ -1,34 +1,54 @@
 package core;
 
-import java.io.IOException;
-import java.util.List;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectReader;
 import com.fasterxml.jackson.databind.ObjectWriter;
+
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FileHandling {
 
-    public Player player;
+    public static Player player1 = new Player("testPlayer1", 10000);
+    public static Player player2 = new Player("testPlayer2", 600);
+    public static Player player3 = new Player("testPlayer3", 4000);
+    public static Player player4 = new Player("testPlayer4", 200);
+
+    public static Map<String, Player> playerDB = new HashMap<String, Player>();
 
     public void readData() {
-        ObjectMapper mapper = new ObjectMapper();
-
-        try {
-            player = mapper.readValue(new File("playerData.json"), Player.class);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void writeData() {
-        ObjectMapper mapper = new ObjectMapper();
+        //ObjectMapper mapper = new ObjectMapper();
         
         try {
-            mapper.writeValue(new File("playerData.json"), player);
+            //Map<?, ?> tempMap = mapper.readValue(Paths.get("casino/resources/playerDB.json").toFile(), Map.class);
+            
+            playerDB.clear();
 
-            String jsonString = mapper.writeValueAsString(player);
-            System.out.println(jsonString);
-        } catch (IOException e) {
+            /*for(Map.Entry<?, ?> entry : tempMap.entrySet()) {
+                playerDB.put(entry.getKey(), entry.getValue()); //Bug?
+            }*/
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
+
+    public static void writeData() { //Functional
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectWriter writer = mapper.writer(new DefaultPrettyPrinter());
+
+        playerDB.put(player1.getName(), player1);
+        playerDB.put(player2.getName(), player2);
+        playerDB.put(player3.getName(), player3);
+        playerDB.put(player4.getName(), player4);
+
+        try {
+            writer.writeValue(Paths.get("casino/resources/playerDB.json").toFile(), playerDB);
+            System.out.println("Player data base written");
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
