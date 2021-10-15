3a8e3b812f6f07eabd06a9fd196979bacfe5dc6c
Petter Næss Evjen <petterev@stud.ntnu.no>
2021-10-15 14:45:30 +0200

    Re added test changes

--- casino/ui/src/test/java/ui/LogInTest.java
+++ casino/ui/src/test/java/ui/LogInTest.java
@@ -23,27 +23,43 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.testfx.framework.junit5.ApplicationTest;
 import org.testfx.matcher.control.LabeledMatchers;
 
-
-public class LogInTest extends ApplicationTest{
+public class LogInTest extends ApplicationTest {
 
     private AppController controller;
 
     @Override
     public void start(final Stage stage) throws Exception {
-    final FXMLLoader loader = new FXMLLoader(getClass().getResource("LogIn.fxml"));
-    final Parent root = loader.load();
-    this.controller = loader.getController();
-    final Scene scene = new Scene(root);
-    stage.setScene(scene);
-    stage.show();
+        final FXMLLoader loader = new FXMLLoader(getClass().getResource("LogIn.fxml"));
+        final Parent root = loader.load();
+        this.controller = loader.getController();
+        final Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @Test
-    public void testController(){
+    public void testController() {
         assertNotNull(this.controller);
     }
 
+    @Test
+    public void testingError() {
+        String emptyText = "";
+        clickOn("#signInUser").write(emptyText);
+        assertEquals(emptyText, controller.getUsername());
 
+    }
 
-   
+    @Test
+    public void testingNameAndPassword() {
+        String firstName = "Erna Solberg";
+        String surname = "Solberg";
+        String hidden = "???????";
+        clickOn("#signInUser").write(firstName);
+        clickOn("#signInPassword").write(surname);
+        assertEquals(firstName, controller.getUsername());
+        assertNotEquals(hidden, controller.getPassword());
+
+    }
+
 }
